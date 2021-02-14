954b75d3607ee99f21b18e78462b0f9dae8520d9
Nick-Student <60278714+Nick-Student@users.noreply.github.com>
2021-02-13 18:26:37 -0600

    Refactored for separate screens and sidebar

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,21 @@
 import 'package:flutter/material.dart';
 // import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter_project/map.dart';
+import 'file:///C:/Users/19407/AndroidStudioProjects/flutter_project/lib/screens/map.dart';
 //import 'package:location/location.dart';
+import 'package:flutter_project/screens/home.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mavericks Abroad',
+      title: "Mavericks Abroad",
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: MyHomePage(title: 'Mavericks Abroad'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'In Development',
-                style: TextStyle(fontSize: 42),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Imagine a really sleek homepage in the meantime',
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Increment',
-        child: Icon(Icons.map),
-        onPressed: () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => GMap()),
-        ),
-      ),
+      home: Home(title: 'Mavericks Abroad'),
     );
   }
 }
