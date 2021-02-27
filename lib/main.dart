3e71c112a2e70768a1268421b435a5f8a174a264
Nick-Student <nickpsoliz@gmail.com>
2021-02-26 18:28:28 -0600

    Updated weather BG
    Full page background for weather

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Mavericks Abroad",
+        debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,39 +12,45 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.transparent,
       appBar: AppBar(title: Text('Mavericks Abroad')),
       drawer: SideDrawer(),
-      body: Column(children: <Widget> [
-        Align(
-          alignment: Alignment.topCenter,
-          child: Image(
-            height: 400,
-            width: 350,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
             image: NetworkImage(
-                'https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20201116111704&q=80&rw=750&rh=536'
-            )
-          )
-        ),
-        Text(
-            'Weather Information Displayed here.',
-          style: TextStyle(fontSize: 24)
-        ),
-        Text(
-          'Picture will update based on forecast.'
-        )
+                //'https://github.com/mercihohmann/flutter-weather-app-starterkit/blob/master/weather_app/images/heavyrain.png?raw=true'
+                //'https://github.com/mercihohmann/flutter-weather-app-starterkit/blob/master/weather_app/images/thunderstorm.png?raw=true'
+                //'https://github.com/mercihohmann/flutter-weather-app-starterkit/blob/master/weather_app/images/clear.png?raw=true'
+                //'https://github.com/mercihohmann/flutter-weather-app-starterkit/blob/master/weather_app/images/lightcloud.png?raw=true'
+                'https://s1.1zoom.me/b5373/148/Japan_Winter_Roads_Shirakawa-go_and_Gokayama_Snow_561175_1080x1920.jpg'
+                //'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU7giuE81IEhjIJlGiVAPNeOuL9VC52gZalQ&usqp=CAU'
+                //'https://wallpaperaccess.com/full/691908.jpg'
+                //'https://lh3.googleusercontent.com/proxy/CvnjPSBPdZ9rQrs5Fru9IpbxqmHSBxA2yxKTuafQU0Novk33nfL_eOLvU6jP4cit5RPBs-HA81JvDvyG-oHh6tXJ2Jnbqb1EoRN_GvNBf8GvM8dJwP7e-qM=s0-d'
 
-        
-      ],),
 
-        // floatingActionButton: FloatingActionButton(
-        //   tooltip: 'Open the Map',
-        //   child: Icon(Icons.map),
-        //   onPressed: () => Navigator.push(
-        //     context,
-        //     MaterialPageRoute(builder: (context) => GMap()),
-        //   ),
-        // ),
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget> [
+              Text(
+                  'Weather Information Displayed here.',
+                style: TextStyle(fontSize: 24,color: Colors.deepPurple[700], backgroundColor: Colors.blueGrey),
+
+
+              ),
+              Text(
+                'Picture will update based on forecast.',
+                style: TextStyle(fontSize: 16,color: Colors.deepPurple[700], backgroundColor: Colors.blueGrey),
+              )
+            ],),
+        ),
+      ),
     );
   }
 }
