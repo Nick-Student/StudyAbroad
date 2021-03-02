425c470af74c39c46b239dcc24e95b87b4a9dbe4
Nick-Student <60278714+Nick-Student@users.noreply.github.com>
2021-03-01 20:58:06 -0600

    updated with weather bg

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -24,7 +24,7 @@ class _HomeState extends State<Home> {
                 //'https://github.com/mercihohmann/flutter-weather-app-starterkit/blob/master/weather_app/images/clear.png?raw=true'
                 //'https://github.com/mercihohmann/flutter-weather-app-starterkit/blob/master/weather_app/images/lightcloud.png?raw=true'
                 'https://s1.1zoom.me/b5373/148/Japan_Winter_Roads_Shirakawa-go_and_Gokayama_Snow_561175_1080x1920.jpg'
-                //'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU7giuE81IEhjIJlGiVAPNeOuL9VC52gZalQ&usqp=CAU'
+                ////////'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU7giuE81IEhjIJlGiVAPNeOuL9VC52gZalQ&usqp=CAU'
                 //'https://wallpaperaccess.com/full/691908.jpg'
                 //'https://lh3.googleusercontent.com/proxy/CvnjPSBPdZ9rQrs5Fru9IpbxqmHSBxA2yxKTuafQU0Novk33nfL_eOLvU6jP4cit5RPBs-HA81JvDvyG-oHh6tXJ2Jnbqb1EoRN_GvNBf8GvM8dJwP7e-qM=s0-d'
 
@@ -47,6 +47,10 @@ class _HomeState extends State<Home> {
               Text(
                 'Picture will update based on forecast.',
                 style: TextStyle(fontSize: 16,color: Colors.deepPurple[700], backgroundColor: Colors.blueGrey),
+              ),
+              Text(
+                'We are meeting at the park today!!!!',
+                style: TextStyle(fontSize: 20, color: Colors.black, height: 32,backgroundColor: Colors.blueGrey)
               )
             ],),
         ),
