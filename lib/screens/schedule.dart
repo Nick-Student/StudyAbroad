a9014e6e94ea10879d4600efca1fe70116fddbb3
sth8827 <54602497+sth8827@users.noreply.github.com>
2021-03-01 20:04:46 -0600

    Calendar Functionality

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -1,1 +1,21 @@
 // Scheduler
+import 'package:flutter/cupertino.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+@override
+void initState() {
+  super.initState();
+  _calendarController = CalaendarController();
+}
+
+@override
+void dispose() {
+  _calendarController.dispose();
+  super.dispose();
+}
+
+@override
+Widget build(BuildContext context) {
+  return TableCalendar(calendarController: _calendarcontroller,)
+}
+
