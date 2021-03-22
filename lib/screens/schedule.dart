df2d70e58b1c9ea32245aa8bd40a05dd6fabd4de
sth8827 <54602497+sth8827@users.noreply.github.com>
2021-03-22 11:54:10 -0500

    Update schedule.dart

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -2,27 +2,40 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+
+
 class Scheduler extends StatefulWidget {
   @override
   _SchedulerState createState() => _SchedulerState();
 }
-
+//Connecting screen to scheduler button
 class _SchedulerState extends State<Scheduler> {
+  //Map<DateTime, List> _events;
   CalendarController _controller;
   @override
   void initState() {
     super.initState();
     _controller = CalendarController();
+    //_events = {};
   }
-  @override
+  @override //Creation of calendar
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Calendar'),
+        title: Text('Calendar'),
       ),
-      body: Stack(children: <Widget>[
-        TableCalendar(calendarController: _controller)
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(
+              events: _events,
+              headerStyle: HeaderStyle(
+                centerHeaderTitle: true, //Centering the calendar title
+              ),
+              calendarController: _controller,)
+          ]
+        )
       ),
     );
   }
