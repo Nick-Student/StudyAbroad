55aabee0c2c06d00c801a2bcd0b55f0f36a7b932
sth8827 <54602497+sth8827@users.noreply.github.com>
2021-04-05 00:01:55 -0500

    integrating audio buttons

--- lib/screens/phrases.dart
+++ lib/screens/phrases.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 // The Item class will hold store the state information for the expansion panel.
 class Item {
-  Item(this.englishPhrase, this.romajiPhrase, this.japanesePhrase, this.isExpanded); // Constructor
+  Item(this.englishPhrase, this.romajiPhrase, this.japanesePhrase, this.audioPhrase, this.isExpanded); // Constructor
 
   String englishPhrase; // The englishPhrase variable will hold the string for an English sentence.
   String romajiPhrase; // The romajiPhrase variable will hold the string for a romanized sentence.
   String japanesePhrase; // The japanesePhrase variable will hold the string for a Japanese sentence.
+  String audioPhrase;
   bool isExpanded; // The isExpanded variable will determine the state of the panel for expansion and collapsion.
 }
 
@@ -17,11 +20,19 @@ class Phrases extends StatefulWidget {
 
 class _PhrasesState extends State<Phrases> {
 
+  AudioCache _audioCache;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioCache = AudioCache(prefix: "audio/", fixedPlayer: AudioPlayer()..setReleaseMode(ReleaseMode.STOP));
+  }
+
   // The _phrases list will contain all the phrases. If a phrase needs to be added or deleted, it can be done here.
   final List<Item> _phrases = [
-    Item("Hello", "konnichiwa", "こんにちは", false),
-    Item("Good Morning", "ohayou gozaimasu", "おはようございます", false),
-    Item("Goodbye", "sayounara", "さようなら", false),
+    Item("Hello", "konnichiwa", "こんにちは", "こんにちは.mp3", false),
+    Item("Good Morning", "ohayou gozaimasu", "おはようございます", "おはようございます.mp3", false),
+    Item("Goodbye", "sayounara", "さようなら", "さようなら.mp3", false),
   ];
 
   @override
@@ -60,7 +71,15 @@ class _PhrasesState extends State<Phrases> {
           // The romanized and Japanese phrases will only be visible when the panel is in its expanded state.
           body: ListTile(
             title: Text(item.romajiPhrase),
-            subtitle: Text(item.japanesePhrase),
+            subtitle: Column(
+              children: <Widget>[
+                Text(item.japanesePhrase),
+                RaisedButton(
+                  onPressed: () => _audioCache.play(item.audioPhrase),
+                  child: Text("Play Audio"),
+                ),
+              ],
+            ),
           ),
           isExpanded: item.isExpanded,
         );
