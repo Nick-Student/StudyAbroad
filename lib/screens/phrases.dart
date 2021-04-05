f39854e5715fa1c1ad56b5321bfe7c59663d65d0
bwin4 <56770906+bwin4@users.noreply.github.com>
2021-04-05 13:48:31 -0500

    Integrated audio feature for phrases

--- lib/screens/phrases.dart
+++ lib/screens/phrases.dart
@@ -4,12 +4,12 @@ import 'package:audioplayers/audioplayers.dart';
 
 // The Item class will hold store the state information for the expansion panel.
 class Item {
-  Item(this.englishPhrase, this.romajiPhrase, this.japanesePhrase, this.audioPhrase, this.isExpanded); // Constructor
+  Item(this.englishPhrase, this.romajiPhrase, this.japanesePhrase, this.audioFile, this.isExpanded); // Constructor
 
   String englishPhrase; // The englishPhrase variable will hold the string for an English sentence.
   String romajiPhrase; // The romajiPhrase variable will hold the string for a romanized sentence.
   String japanesePhrase; // The japanesePhrase variable will hold the string for a Japanese sentence.
-  String audioPhrase;
+  String audioFile; // The audioFile variable will hold the string for the file name of the audio file.
   bool isExpanded; // The isExpanded variable will determine the state of the panel for expansion and collapsion.
 }
 
@@ -19,21 +19,39 @@ class Phrases extends StatefulWidget {
 }
 
 class _PhrasesState extends State<Phrases> {
-
   AudioCache _audioCache;
+
+  // The _phrases list will contain all the phrases. If a phrase needs to be added or deleted, it can be done here.
+  final List<Item> _phrases = [
+    Item("Hello / Good Afternoon", "konnichiwa", "こんにちは", "hello.mp3", false),
+    Item("Good Morning", "ohayou gozaimasu", "おはようございます", "morning.mp3", false),
+    Item("Good Evening", "konbanwa", "こんばんは", "evening.mp3", false),
+    Item("Goodbye", "sayounara", "さようなら", "goodbye.mp3", false),
+    Item("Please", "onegai shimasu", "おねがいします", "please.mp3", false),
+    Item("Thank you","arigatou gozaimasu", "ありがとうございます", "thanks.mp3", false),
+    Item("You're welcome","dou itashimashite", "どういたしまして", "welcome.mp3", false),
+    Item("Excuse me / Sorry","sumimasen", "すみません", "sorry.mp3", false),
+    Item("Are you okay?", "daijyoubu desu ka?", "だいじょうぶ ですか？", "okay.mp3", false),
+    Item("Yes","hai", "はい", "yes.mp3", false),
+    Item("No","iie", "いいえ", "no.mp3", false),
+    Item("I don't understand","wakarimasen", "わかりません", "understand.mp3", false),
+    Item("Can you help me?","tasukete moraemasuka?", "たすけて もらえますか？", "help.mp3", false),
+    Item("Where is (_____)?","(_____) wa doko desu ka?", "(_____)はどこですか？", "where.mp3", false),
+    Item("I want to go to (_____)","(_____) ni ikitai desu", "(_____)に いきたいです", "togo.mp3",   false),
+    Item("Toilet", "toire", "トイレ", "toilet.mp3", false),
+    Item("This hotel", "kono hoteru", "この ほてる", "hotel.mp3", false),
+    Item("The train station", "denshya no eki", "でんしゃの えき", "train.mp3", false),
+    Item("The bus station", "basutei", "ばすてい", "bus.mp3", false),
+    Item("Take me to this address, please","kono jyuushyo made tsureteitte kudasai", "この じゅうしょ まで つれていってください", "address.mp3", false),
+    Item("How much does this cost?","kore wa ikura desu ka?", "これは いくらですか？", "cost.mp3", false),
+    Item("I would like this","kore o onegai shimasu", "これを おねがいします", "want.mp3", false),
+  ];
 
   @override
   void initState() {
     super.initState();
-    _audioCache = AudioCache(prefix: "audio/", fixedPlayer: AudioPlayer()..setReleaseMode(ReleaseMode.STOP));
+    _audioCache = AudioCache(prefix: "audio/"); // The file will be fetched from the local audio folder.
   }
-
-  // The _phrases list will contain all the phrases. If a phrase needs to be added or deleted, it can be done here.
-  final List<Item> _phrases = [
-    Item("Hello", "konnichiwa", "こんにちは", "こんにちは.mp3", false),
-    Item("Good Morning", "ohayou gozaimasu", "おはようございます", "おはようございます.mp3", false),
-    Item("Goodbye", "sayounara", "さようなら", "さようなら.mp3", false),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -71,15 +89,14 @@ class _PhrasesState extends State<Phrases> {
           // The romanized and Japanese phrases will only be visible when the panel is in its expanded state.
           body: ListTile(
             title: Text(item.romajiPhrase),
-            subtitle: Column(
-              children: <Widget>[
-                Text(item.japanesePhrase),
-                RaisedButton(
-                  onPressed: () => _audioCache.play(item.audioPhrase),
-                  child: Text("Play Audio"),
-                ),
-              ],
-            ),
+            subtitle: Text(item.japanesePhrase),
+            trailing: ElevatedButton(
+              onPressed: () => _audioCache.play(item.audioFile), // The audio file will play when the button is pressed.
+              child: Icon(
+                Icons.volume_up_rounded,
+                color:Colors.lightBlue,
+              ),
+            )
           ),
           isExpanded: item.isExpanded,
         );
